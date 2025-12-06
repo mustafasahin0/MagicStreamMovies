d94aa6d996302b96f17b95cd0adf30e3b48f9a2f
mustafasahin0 <sahinmustafaenes4@gmail.com>
2025-12-06 14:46:50 -0700

    Refactor database connection and movie retrieval logic; update dependencies and environment configuration

--- Server/MagicStreamMoviesServer/database/database_connection.go
+++ Server/MagicStreamMoviesServer/database/database_connection.go
@@ -5,12 +5,57 @@ import (
 	"log"
 	"os"
 
-
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
-
 )
 
 func DBInstance() *mongo.Client {
 
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Warning: Unable to load .env file")
+	}
+
+	MongoDb := os.Getenv("MONGODB_URI")
+
+	if MongoDb == "" {
+		log.Fatal("MONGODB_URI not set")
+	}
+
+	fmt.Println("MongoDB URI: ", MongoDb)
+
+	clientOptions := options.Client().ApplyURI(MongoDb)
+
+	client, err := mongo.Connect(clientOptions)
+	
+	if err != nil {
+		return nil
+	}
+	
+	return client
 }
+
+var Client *mongo.Client = DBInstance()
+
+func OpenCollection(collectionName string) *mongo.Collection {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Warning: Unable to load .env file")
+	}
+
+	databaseName := os.Getenv("DATABASE_NAME")
+
+	fmt.Println("Database Name: ", databaseName)
+
+	collection := Client.Database(databaseName).Collection(collectionName)
+
+	if collection == nil {
+		return nil
+	}
+
+	return collection
+}
+
--- Server/MagicStreamMoviesServer/models/movie_modal.go
+++ Server/MagicStreamMoviesServer/models/movie_modal.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type Genre struct {
@@ -15,7 +15,7 @@ type Ranking struct {
 }
 
 type Movie struct {
-	ID          primitive.ObjectID `bson:"_id" json:"_id"`
+	ID          bson.ObjectID `bson:"_id" json:"_id"`
 	ImdbID      string             `bson:"imdb_id" json:"imdb_id" validate:"required"`
 	Title       string             `bson:"title" json:"title" validate:"required,min=2,max=500"`
 	PosterPath  string             `bson:"poster_path" json:"poster_path" validate:"required,url"`
