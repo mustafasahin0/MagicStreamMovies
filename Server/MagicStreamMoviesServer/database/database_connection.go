2718ebbbb8295f2c643ac1e9318741ba3f703dff
mustafasahin0 <sahinmustafaenes4@gmail.com>
2025-12-06 14:07:53 -0700

    first commit

---
+++ Server/MagicStreamMoviesServer/database/database_connection.go
@@ -0,0 +1,16 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+
+)
+
+func DBInstance() *mongo.Client {
+
+}
